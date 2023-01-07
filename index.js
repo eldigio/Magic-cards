8687fee90371d196dacf72cc4ca05362b150e0dc
Cristian <cristianguaglianone@gmail.com>
2023-01-07 18:08:32 +0100

    starter project

---
+++ index.js
@@ -0,0 +1,34 @@
+"use-strict";
+
+////////////////////// DATA ////////////////////////////
+const deck = [1, 3, 2, 5, 3, 9, 2, 8];
+
+////////////////////// SELECTORS ////////////////////////////
+const cardContainer = document.querySelector(".card_container");
+const magicButtons = document.querySelector(".magic_buttons");
+const doubleNumber = document.querySelector(".double_number");
+const findMiddle = document.querySelector(".find_middle");
+const find2 = document.querySelector(".find_2");
+const reorderDeck = document.querySelector(".reorder_deck");
+const sumDeck = document.querySelector(".sum_deck");
+const resetButton = document.querySelector(".reset");
+
+////////////////////// FUNCTIONS ////////////////////////////
+
+const initialize = () => {};
+
+////////////////////// EVENT LISTENERS ////////////////////////////
+
+// Raddoppia il valore
+
+// Trova le carte centrali
+
+// Mantieni i 2
+
+// Riordina al contrario
+
+// Somma carte
+
+// Reset
+
+initialize();
