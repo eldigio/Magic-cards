86a44b09f2f60792f8fcde56263380365ae07ba9
ElDiGio <samuel.dgv@gmail.com>
2023-02-02 09:09:27 +0100

    first commit | original

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-"use-strict";
+'use-strict';
 
 ////////////////////// DATA ////////////////////////////
 const deck = [1, 3, 2, 5, 3, 9, 2, 8];
 
 ////////////////////// SELECTORS ////////////////////////////
-const cardContainer = document.querySelector(".card_container");
-const magicButtons = document.querySelector(".magic_buttons");
-const doubleNumber = document.querySelector(".double_number");
-const findMiddle = document.querySelector(".find_middle");
-const find2 = document.querySelector(".find_2");
-const reorderDeck = document.querySelector(".reorder_deck");
-const sumDeck = document.querySelector(".sum_deck");
-const resetButton = document.querySelector(".reset");
+const cardContainer = document.querySelector('.card_container');
+const magicButtons = document.querySelector('.magic_buttons');
+const doubleNumber = document.querySelector('.double_number');
+const findMiddle = document.querySelector('.find_middle');
+const find2 = document.querySelector('.find_2');
+const reorderDeck = document.querySelector('.reorder_deck');
+const sumDeck = document.querySelector('.sum_deck');
+const resetButton = document.querySelector('.reset');
 
 ////////////////////// FUNCTIONS ////////////////////////////
 
