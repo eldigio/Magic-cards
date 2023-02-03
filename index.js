754561e963ef93c9b1d486e59663bde5af4fa939
ElDiGio <samuel.dgv@gmail.com>
2023-02-03 20:12:33 +0100

    finished

--- index.js
+++ index.js
@@ -12,22 +12,80 @@ const find2 = document.querySelector('.find_2');
 const reorderDeck = document.querySelector('.reorder_deck');
 const sumDeck = document.querySelector('.sum_deck');
 const resetButton = document.querySelector('.reset');
-// cardContainer.firstChild.innerHTML = '';
 
 ////////////////////// FUNCTIONS ////////////////////////////
+const deckSum = () => {
+  return deck.reduce((acc, currValue) => acc + currValue, 0);
+};
 
-const reset = () => {
-  deck.forEach(value => {
-    const newNode = document.createElement('div');
-    newNode.classList.add('card');
-    newNode.textContent = value;
-    cardContainer.appendChild(newNode);
+const card = textContent => {
+  const newNode = document.createElement('div');
+  newNode.classList.add('card');
+  newNode.textContent = textContent;
+  cardContainer.appendChild(newNode);
+};
+const clearCardContainer = () => (cardContainer.innerHTML = '');
+const createCards = (arr = deck) => {
+  arr.forEach(value => {
+    card(value);
   });
 };
 
+const hideBtns = () => {
+  document.querySelectorAll('.btn').forEach(value => {
+    if (value.classList[1] !== 'reset') value.style.display = 'none';
+  });
+};
+
+const showBtns = () => {
+  document.querySelectorAll('.btn').forEach(value => {
+    if (value.classList[1] !== 'reset') value.style.display = 'block';
+  });
+};
+
+const reset = () => {
+  clearCardContainer();
+  createCards();
+  showBtns();
+};
+
 const double = () => {
-  const value = cardContainer.childNodes.forEach(value => value.textContent);
-  console.log(value);
+  hideBtns();
+  cardContainer.childNodes.forEach(value => (value.textContent *= 2));
+};
+
+const findOnly2 = () => {
+  clearCardContainer();
+  hideBtns();
+  deck.forEach(value => {
+    if (value === 2) {
+      card(value);
+    }
+  });
+};
+
+const reverseOrder = () => {
+  clearCardContainer();
+  hideBtns();
+  createCards([...deck].reverse());
+};
+
+const findCentral = () => {
+  clearCardContainer();
+  hideBtns();
+  const middle = deck[Math.floor(deck.length / 2)];
+  console.log(middle);
+  deck.forEach((value, index) => {
+    if (index === middle || index === middle + 1) {
+      card(value);
+    }
+  });
+};
+
+const sumCards = () => {
+  clearCardContainer();
+  hideBtns();
+  card(deckSum());
 };
 
 ////////////////////// EVENT LISTENERS ////////////////////////////
@@ -36,12 +94,18 @@ const double = () => {
 doubleNumber.addEventListener('click', double);
 
 // Trova le carte centrali
+findMiddle.addEventListener('click', findCentral);
 
 // Mantieni i 2
+find2.addEventListener('click', findOnly2);
 
 // Riordina al contrario
+reorderDeck.addEventListener('click', reverseOrder);
 
 // Somma carte
+sumDeck.addEventListener('click', sumCards);
 
 // Reset
 reset();
+showBtns();
+resetButton.addEventListener('click', reset);
